f647dd9bb0752b401e17b89b03fd64e5067fc5c8
Jakub Rzempała <rzempala.jakub@gmail.com>
2026-03-14 00:03:33 +0100

    Created test data files

---
+++ test-data/products.ts
@@ -0,0 +1,8 @@
+export const products = {
+    backpack: 'Sauce Labs Backpack',
+    bike: 'Sauce Labs Bike Light',
+    tshirt: 'Sauce Labs Bolt T-Shirt',
+    jacket: 'Sauce Labs Fleece Jacket',
+    onesie: 'Sauce Labs Onesie',
+    longsleeve: `Test.allTheThings() T-Shirt (Red)`
+};
